525fbfb33a859e85cb9cf3dc66f44f2627194f31
league <league@imac-13.attlocal.net>
2021-10-08 17:00:37 -0700

    four square, flaming ninja star

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -10,36 +10,37 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
-
+		Robot geoff =new Robot ();
+		geoff.penDown();
 		// Set the robot speed to 100
-		
+		geoff.setSpeed(100);
 		// Set the robot window size to 800 x 800
-
+		
 		//  COUNT. Make an int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-		
+		int squaresDrawn = 0;
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+		while(squaresDrawn<10) {
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+			geoff.turn(45);
 			   // MOVE           Move the robot 64 pixels
-
+		geoff.move(64);
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			geoff.turn(-40);
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
+			geoff.move(flameSize);
 				//               Turn the robot 170 degrees
-			
+			geoff.turn(170);
 				//               Move the robot the distance in the variable flameSize (again)
-			
+			geoff.move(flameSize);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+			geoff.turn(64);
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+		geoff.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
-		
+		squaresDrawn=squaresDrawn+1;
 		//  End the while loop here
-		
+		}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
 		
--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,39 +6,42 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// Create a new Robot
-
+Robot geoff =new Robot ();
 	void go() {
 		
 		// Make the robot move quickly (setSpeed)
-
+geoff.setSpeed(50);
 		// Set the robot's pen width to 5  
-		
+		geoff.setPenWidth(10);
 		// Put the robot's pen down
-		
+		geoff.penDown();
 		// This numberOfSquares variable will track how many squares the robot has drawn
 		// It's set to zero here, because the robot hasn't drawn any squares yet.
 		int squaresDrawn = 0;
 
 		// LOOP: Start a while loop to repeat the following code until 4 squares have been drawn:
-
+while(squaresDrawn<10) {
 				// PEN COLOR.     Set the pen color to random       setRandomPenColor()
-		
+	geoff.setRandomPenColor();
 				// DRAW A SQUARE. Call the drawSquare() method.  Have you put some code in it?
-		
+	drawSquare();
 				// TURN.          Turn the robot 90 degrees to the right
-			
+			geoff.turn(90);
 			    // INCREASE COUNT. Add one to the number of squares drawn
-			
+			squaresDrawn=squaresDrawn+1;
 		// End the loop here
-	}
+	
+}}
 
 	
 	void drawSquare() {
 		System.out.println("Yay! you called the drawSquare() method!");
 
 		/*  Put code here to draw one square - don't forget to use a loop! */
-		
-		
+		for(int i=0; i<4;i++) {
+			geoff.move(50);
+			geoff.turn(90);
+		}
 	}
 
 	public static void main(String[] args) {
