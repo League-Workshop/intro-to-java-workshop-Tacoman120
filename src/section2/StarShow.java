6b0b7e3dbebf1f8b9e70ea6e99de483abb4d8204
league <league@imac-13.attlocal.net>
2021-10-22 17:02:28 -0700

    smart shapes spiral flamingninjastsr

--- src/section2/StarShow.java
+++ src/section2/StarShow.java
@@ -17,31 +17,31 @@ public class StarShow {
 		// This code will draw one star whose size is 150 pixels per side....
 		// but first you have to write some code. Find the drawStar() method and 
 		// write the code to draw a star.
-		drawStar(150);
+		
 		
 		// Run the program. Check that your star is the same as the first picture in the recipe.
 		// Remove the drawStar(150) from the program before you proceed.
 		
 		// Set the robot speed to 100
-
+batman.setSpeed(100);
 		// Make a variable to hold the X position of the Robot and set it to 10
-
+int x = 10;
 		// Make a variable to hold the Y position of the Robot and set it to 600
-
+int y = 600;
 		// Make a variable to hold the star size and set it to 25
-	
+	int starsize = 25;
 		// Make a variable to count how many stars have been drawn and set it to 0
-
+int stars = 0;
 		// LOOP: Start a while loop to repeat the following code until 7 stars have been drawn
-
+while(stars<7);
 			// Set the pen width to the number of stars drawn so far
-
+batman.setPenWidth(0);
 			// Set the X position of the robot to your X variable
-
+batman.setX(x);
 			// Set the Y position of the robot to your Y variable
-
+batman.setY(y);
 			// Call the drawStar() method with your star size variable
-
+drawStar(starsize);
 			// Increase the value of the X position variable by star size. See Figure 2
 
 			// decrease the value of the Y position variable by star size. See Figure 3
