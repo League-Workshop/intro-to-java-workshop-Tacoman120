dfc0a3c465342b0e3a46b9db22e8b2fa2278d681
league <league@imac-13.attlocal.net>
2021-11-12 17:01:12 -0800

    Candy Crush, Madlibs of The Amazon, Speak and spell

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -8,19 +8,23 @@ import com.sun.speech.freetts.VoiceManager;
 public class SpeakAndSpell {
 	
 	/***********  SOUND ***************
-	 * This recipe requires a computer that can play sounds. 
-	 * If you cannot play sound on this computer, skip this recipe.
-	 * If you are not sure, ask your teacher 
+	 * This recipe requires a comn this computer, skip this recipe.
+	 * If you are not sure, ask puter that can play sounds. 
+	 * If you cannot play sound oyour teacher 
 	 * *****************/
 
 	
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("spell Icup");
 		// 2. Catch the user's answer in a String
-
+String Question = JOptionPane.showInputDialog("spell Icup");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if (Question.equals( "I c u p")) {
+	speak("correct");
+} else {
+	speak(" wrong");
+}
 		// 4. Otherwise say "wrong"
 
 		// 5. repeat the process for other words
