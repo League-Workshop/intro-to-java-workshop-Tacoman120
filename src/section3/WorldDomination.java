2a439d1e1b3735a18fc4889d9f5317c1c2db335f
league <league@imac-13.attlocal.net>
2021-12-03 16:53:30 -0800

    Awsome or not, Dragon Fight

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,9 +6,13 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+String Question = JOptionPane.showInputDialog(null,"Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (Question.equals("yes")) {
+	 JOptionPane.showMessageDialog(null,"You will rule the world with the power of Taco Bell.");
+}else {
+	String Quest = JOptionPane.showInputDialog(null,"Wash some dishes then.");
+}
 		// 3. Otherwise, wish them good luck washing dishes.
 
 	}
--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,18 +2,29 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	int joe = 0;
+	joe = new Random().nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(joe);
 	// 4. Get the user to enter something that they think is awesome
-
+String Gryffin = JOptionPane.showInputDialog(null, "Type some thing you think is awesome.");
 	// 5. If the random number is 0
-
+if (joe==0) {
+	JOptionPane.showMessageDialog(null, "that is Awsome");
+}else if (joe==1) {
+	JOptionPane.showMessageDialog(null, "that is.. ok");
+}else if (joe==2) {
+	JOptionPane.showMessageDialog(null, "that is BORING");
+}else if (joe==3) {
+	JOptionPane.showMessageDialog(null, "you are a NOOB");
+}
 	// -- tell the user whatever they entered is awesome!
 
 	// 6. If the random number is 1
@@ -27,6 +38,7 @@ public class AwesomeOrNot {
 	// 8. If the random number is 3
 
 	// -- write your own answer
+}
 
 }
 
